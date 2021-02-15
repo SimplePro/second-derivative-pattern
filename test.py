f638154549f03cc74e39638a7fde73b1e44e1b97
DonghwanKim <66504341+SimplePro@users.noreply.github.com>
2021-02-15 19:37:38 +0900

    added test file

---
+++ test.py
@@ -0,0 +1,13 @@
+from derivate_pattern import Functions
+
+# x^2 + 3x - 2
+# f``(x) = 2
+# f(1) = 2
+# f(-1) = -4
+functions = Functions(h=2)
+f1 = (-1, -4)
+f2 = (1, 2)
+functions.add_func(f1)
+functions.add_func(f2)
+functions.t()
+print(functions.y(x=48124))
